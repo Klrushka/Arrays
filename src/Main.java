c550ca14c2ce86ac1294026fc620bad4204e448d
Klrushka <comeindonotbeshy@gmail.com>
2021-08-17 11:50:02 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        /*
+        Exercise 1: (2) Create a method that takes an array of BerylliumSphere as an
+        argument. Call the method, creating the argument dynamically. Demonstrate that ordinary
+        aggregate array initialization doesn’t work in this case. Discover the only situations where
+        ordinary aggregate array initialization works, and where dynamic aggregate initialization is
+        redundant.
+         */
+
+
+
+
+
+
+
+
+    }
+}
